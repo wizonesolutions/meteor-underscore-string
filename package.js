22267f7c82a221275acf30e25ef1423e343fd73f
Tim Heckel <tim@slatebox.com>
2013-08-19 17:51:15 -0500

    Prepping for atmosphere

--- package.js
+++ package.js
@@ -3,15 +3,14 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.use('meteor', {unordered: true});
+  	api.use('meteor', {unordered: true});
 	api.use('underscore', ['client', 'server']);
 
 	api.export('_s');
 
-  api.add_files(['pre.js','lib/underscore.string/lib/underscore.string.js','post.js']);
+  	api.add_files(['pre.js','lib/underscore.string/lib/underscore.string.js','post.js']);
 
 	if (typeof api.export !== 'undefined') {
 		api.use('webapp', 'server');
 	}
-
 });
